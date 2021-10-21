3254b14fb2e337b855d6d143f8880c62ace63b3a
Roy <91334448+rzelayamakingsense@users.noreply.github.com>
2021-10-21 14:59:11 -0500

    [CIOX-75] adding okta environments for prod build (#3)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,14 @@
 export const environment = {
   production: true,
   mock: false,
-  host: "http://localhost:5000",
-  api: "api",
-  version: "",
+  host: 'http://localhost:5000',
+  api: 'api',
+  version: '',
 
   OKTA_ISSUER: 'https://dev-73966756.okta.com/oauth2/default',
   OKTA_CLIENT_ID: '0oa20tfim5H98fdz25d7',
   OKTA_TESTING_DISABLEHTTPSCHECK: false,
   OKTA_REDIRECT_URI: 'http://localhost:4200/callback',
-  OKTA_MESSAGE_URL: 'http://localhost:8000/api/messages'
+  OKTA_MESSAGE_URL: 'http://localhost:8000/api/messages',
+  OKTA_SCOPES: ['openid', 'profile', 'email', 'claims'],
 };
